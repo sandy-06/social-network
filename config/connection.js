46cebd9a301d39116714b2a0d516dcc9e4b9c9ac
Sandra Sanders <sandy-sanders@msn.com>
2022-01-20 12:24:03 -0700

    port

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/db_social-network',{
+    useNewUrlParser: true,  useUnifiedTopology: true,
+});
+
+
+
+
+
+module.exports = mongoose.connection;
